e4cf2e15f16547e0cd13f590155fa9107752eec8
Anthony Ngo <anthony.ngo.dev@gmail.com>
2025-01-30 13:02:18 -0800

    dropdown mixin: add to menu item

--- lib/mixins/to_dropdown.dart
+++ lib/mixins/to_dropdown.dart
@@ -3,10 +3,11 @@ import 'package:flutter/material.dart';
 mixin DropdownMenuItemsMixin {
   List<DropdownMenuItem<dynamic>> toDropdownMenuItemList(List<dynamic> values) {
     return values
-        .map((value) => DropdownMenuItem(
-              value: value,
-              child: Text(value.toString()),
-            ))
+        .map((value) => toDropdownMenuItem(value))
         .toList();
+  }
+
+  DropdownMenuItem<dynamic> toDropdownMenuItem(dynamic value) {
+    return DropdownMenuItem(value: value, child: Text(value.toString()));
   }
 }
