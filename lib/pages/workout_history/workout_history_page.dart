4baca1d456c71b654415f4b3e2fc5c2a1c0ea94c
Anthony Ngo <anthony.ngo.dev@gmail.com>
2025-01-31 18:58:30 -0800

    history page: direct new workout to recording page

--- lib/pages/workout_history/workout_history_page.dart
+++ lib/pages/workout_history/workout_history_page.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:homework/examples/sample_workout_plan.dart';
+import 'package:homework/mixins/navigate_to.dart';
 import 'package:homework/models/workout.dart';
 import 'package:homework/pages/workout_history/workout_history_entry.dart';
+import 'package:homework/pages/workout_recording/workout_recording_page.dart';
+import 'package:homework/widgets/centering/center_text.dart';
 import 'package:homework/widgets/common_scaffold.dart';
 
-class WorkoutHistoryPage extends StatelessWidget {
+class WorkoutHistoryPage extends StatelessWidget with NavigateMixin {
   final List<Workout> workouts;
 
   const WorkoutHistoryPage(this.workouts, {super.key});
@@ -12,13 +16,25 @@ class WorkoutHistoryPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return CommonScaffold(
         title: 'Workout History',
-        content: ListView.separated(
-          itemCount: workouts.length,
-          itemBuilder: (context, index) =>
-              WorkoutHistoryEntry(workouts[index]),
-          separatorBuilder: (context, index) => SizedBox(
-            height: 50,
-          ),
-        ));
+        content:
+          Column(children: [
+            Expanded(
+              child: ListView.separated(
+                itemCount: workouts.length,
+                itemBuilder: (context, index) =>
+                    WorkoutHistoryEntry(workouts[index]),
+                separatorBuilder: (context, index) => SizedBox(
+                  height: 50,
+                ),
+              ),
+            ),
+            FilledButton(
+              onPressed: () => {
+                navigateTo(context: context, widget: WorkoutRecordingPage(sampleWorkoutPlan))
+              }, 
+              child: CenterText('New Workout')
+            )
+          ],)
+      );
   }
 }
