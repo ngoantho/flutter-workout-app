e403b476244f04a1e6885b89fe882384fc34f885
Anthony Ngo <anthony.ngo.dev@gmail.com>
2025-01-31 18:59:28 -0800

    recording: navigate back to history

--- lib/pages/workout_recording/workout_recording_page.dart
+++ lib/pages/workout_recording/workout_recording_page.dart
@@ -1,13 +1,17 @@
 import 'package:date_only/date_only.dart';
 import 'package:flutter/material.dart';
+import 'package:homework/mixins/navigate_to.dart';
 import 'package:homework/models/exercise_result.dart';
 import 'package:homework/models/workout.dart';
+import 'package:homework/pages/test_page.dart';
+import 'package:homework/providers/workout_provider.dart';
 import 'package:homework/widgets/centering/center_text.dart';
 import 'package:homework/widgets/readonly_textfield.dart';
 import 'package:homework/classes/exercise_result_controller.dart';
 import 'package:homework/models/workout_plan.dart';
 import 'package:homework/pages/workout_recording/workout_recording_card.dart';
 import 'package:homework/widgets/common_scaffold.dart';
+import 'package:provider/provider.dart';
 
 class WorkoutRecordingPage extends StatefulWidget {
   final WorkoutPlan workoutPlan;
@@ -18,20 +22,22 @@ class WorkoutRecordingPage extends StatefulWidget {
   State<WorkoutRecordingPage> createState() => _WorkoutRecordingPageState();
 }
 
-class _WorkoutRecordingPageState extends State<WorkoutRecordingPage> {
+class _WorkoutRecordingPageState extends State<WorkoutRecordingPage> with NavigateMixin {
   final _formKey = GlobalKey<FormState>();
   final _today = DateOnly.today();
   bool validated = false;
   late List<ExerciseResultController> _exerciseResultControllers;
 
   void onSave() {
-    final _ = Workout(
+    final workout = Workout(
         date: _today,
         results: _exerciseResultControllers
             .map((controller) => ExerciseResult(
                 exercise: controller.exercise,
                 actualOutput: controller.actualOutput))
             .toList());
+    context.read<WorkoutProvider>().add(workout);
+    navigateBack(context: context);
   }
 
   @override
@@ -48,7 +54,9 @@ class _WorkoutRecordingPageState extends State<WorkoutRecordingPage> {
     return Form(
         key: _formKey,
         autovalidateMode: AutovalidateMode.onUserInteraction,
-        onChanged: () => setState(() => validated = _formKey.currentState!.validate(),),
+        onChanged: () => setState(
+              () => validated = _formKey.currentState!.validate(),
+            ),
         child: CommonScaffold(
             title: 'Record Workout',
             content: Column(children: [
@@ -64,10 +72,9 @@ class _WorkoutRecordingPageState extends State<WorkoutRecordingPage> {
                           _exerciseResultControllers[index].exercise,
                           _exerciseResultControllers[index].controller),
                       itemCount: _exerciseResultControllers.length)),
-              TextButton.icon(
+              FilledButton(
                 onPressed: validated ? onSave : null,
-                label: CenterText('Save Workout'),
-                icon: Icon(Icons.save),
+                child: CenterText('Save Workout'),
               )
             ])));
   }
