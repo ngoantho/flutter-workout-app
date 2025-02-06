0773bc4040830a7bc60394debcf5071719ef0844
Anthony Ngo <anthony.ngo.dev@gmail.com>
2025-02-05 19:03:31 -0800

    add test coverage

---
+++ test/full_coverage_test.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: unused_import
+import 'package:homework/classes/disabled_focus_node.dart';
+import 'package:homework/classes/exercise_result_controller.dart';
+import 'package:homework/examples/sample_workout_plan.dart';
+import 'package:homework/main.dart';
+import 'package:homework/mixins/navigate_to.dart';
+import 'package:homework/mixins/to_dropdown.dart';
+import 'package:homework/mixins/validate_output.dart';
+import 'package:homework/models/exercise.dart';
+import 'package:homework/models/exercise_result.dart';
+import 'package:homework/models/measurement_unit.dart';
+import 'package:homework/models/output.dart';
+import 'package:homework/models/workout.dart';
+import 'package:homework/models/workout_plan.dart';
+import 'package:homework/pages/workout_details/workout_detail_page.dart';
+import 'package:homework/pages/workout_details/workout_result_details.dart';
+import 'package:homework/pages/workout_history/workout_history_entry.dart';
+import 'package:homework/pages/workout_history/workout_history_page.dart';
+import 'package:homework/pages/workout_recording/methods/base_plus_minus_method.dart';
+import 'package:homework/pages/workout_recording/methods/minus_value_plus_method.dart';
+import 'package:homework/pages/workout_recording/methods/value_input_method.dart';
+import 'package:homework/pages/workout_recording/workout_recording_card.dart';
+import 'package:homework/pages/workout_recording/workout_recording_page.dart';
+import 'package:homework/providers/workouts_provider.dart';
+import 'package:homework/widgets/centering/center_column.dart';
+import 'package:homework/widgets/centering/center_row.dart';
+import 'package:homework/widgets/centering/center_text.dart';
+import 'package:homework/widgets/column_scroll_view.dart';
+import 'package:homework/widgets/common_scaffold.dart';
+import 'package:homework/widgets/readonly_textfield.dart';
+import 'package:homework/widgets/recent_performance.dart';
+import 'package:homework/widgets/sized_box/sized_box_with_height.dart';
+import 'package:homework/widgets/sized_box/sized_box_with_width.dart';
+
+void main() {}
